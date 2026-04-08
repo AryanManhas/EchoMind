c31a64a6efd9e0b9ef1414491ec8973087b05f4e
Aryan Manhas <2022A1R033@mietjammu.in>
2026-04-08 18:41:28 +0530

    update files

--- EchoMind/EchoMind-Wearable-Memory-Assistant-main/mobile_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ EchoMind/EchoMind-Wearable-Memory-Assistant-main/mobile_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
+import firebase_messaging
 import record_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
 }
